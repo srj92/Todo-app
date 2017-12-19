9f1054a6f56669fe83682fbefe763fd38eb87f1a
srj92 <suraj.gupta5077@gmail.com>
2017-12-19 22:48:52 +0530

    finished todo app CRUD

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,15 +1,42 @@
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+// Connect to the db
+mongoose.connect('mongodb://<uname>:<pwd>@ds161446.mlab.com:61446/suraj');
+
+// Create a schema
+var todoSchema = new mongoose.Schema({
+    item: String
+});
+
+var Todo = mongoose.model('Todo', todoSchema);
+
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+
 module.exports = function(app) {
 
     app.get('/todo', function(req,res){
-
+        Todo.find({}, function(err, data) {
+            if (err) {
+                throw err;
+            }
+            res.render('todo', {todos: data});
+        });
     });
 
-    app.post('/todo', function(req,res){
-        
+    app.post('/todo', urlencodedParser, function(req,res){
+        var newTodo = Todo(req.body).save(function(err, data) {
+            if (err) throw err;
+            res.json(data);
+        });
     });
 
-    app.delete('/todo', function(req,res){
-        
+    app.delete('/todo/:item', function(req,res){
+        Todo.find({item: req.params.item.replace(/\-/g, " ")}).remove(function(err, data){
+            if (err) {
+                throw err;
+            }
+            res.json(data);
+        });    
     });
-
 };
